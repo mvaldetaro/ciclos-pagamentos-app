85e063a7d6b5315d7b68dff5ce81ba8a7b863c77
Magno Valdetaro <mvaldetaro@gmail.com>
2018-03-28 07:50:47 -0300

    Componente Footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>Magno Valdetaro 2018</strong> - <a href="http://magnovaldetaro.com" target='_blank'>MV</a>
+    </footer>
+)
