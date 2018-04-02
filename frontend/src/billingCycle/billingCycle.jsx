bea2ca4f51e9d01ea1869a15f6014e24969f0244
Magno Valdetaro <mvaldetaro@gmail.com>
2018-04-02 07:26:58 -0300

    Estrutura componente Tabs

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -6,6 +6,10 @@ import {bindActionCreators} from "redux";
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabsContent from '../common/tab/tabsContent'
+
 
 class BillingCycle extends Component {
     render() {
@@ -13,7 +17,14 @@ class BillingCycle extends Component {
             <div>
                 <ContentHeader title='Ciclo de Pagamento' small='Cadastro' />
                 <Content>
-                    Ciclos
+                    <Tabs>
+                        <TabsHeader>
+
+                        </TabsHeader>
+                        <TabsContent>
+                            
+                        </TabsContent>
+                    </Tabs>
                 </Content>
             </div>
         )
