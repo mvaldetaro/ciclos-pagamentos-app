25d27778923ff1ce5ee4f99c199929106d12b232
Magno Valdetaro <mvaldetaro@gmail.com>
2018-04-02 07:43:47 -0300

    Add componete TabHeader

---
+++ frontend/src/common/tab/tabHeader.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+import {connect} from 'react-redux'
+import {bindActionCreators} from "redux";
+
+class TabHeader extends Component {
+    render() {
+        return (
+            <li>
+                <a href='javascript:;' data-toogle='tab' data-target={this.props.target}>
+                    <i className={`fa fa-${this.props.icon}`}></i> {this.props.label}
+                </a>
+            </li>
+        )
+    }
+}
+
+export default TabHeader
