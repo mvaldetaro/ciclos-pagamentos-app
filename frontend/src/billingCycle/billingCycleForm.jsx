bc6aea21fa51d2b54383464b31f9ef5cebcd2d9c
Magno Valdetaro <mvaldetaro@gmail.com>
2018-05-01 21:49:21 -0300

    Alimenta os dados do Summary

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -10,10 +10,19 @@ import Summary from './summary'
 
 
 class BillingCycleForm extends Component {
+
+    calculateSummary() {
+        const sum = (t, v) => t + v
+        return {
+            sumOfCredits:  this.props.credits.map( c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+        }
+    }
+
     render(){
 
         const { handleSubmit, readOnly, credits, debts } = this.props
-
+        const { sumOfCredits, sumOfDebts} = this.calculateSummary()
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className="box-body">
@@ -43,7 +52,7 @@ class BillingCycleForm extends Component {
                         placeholder='Informe o ano'
                         readOnly={readOnly}
                     />
-                    <Summary credit={1000} debt={100} />
+                    <Summary credit={sumOfCredits} debt={sumOfDebts} />
                     <ItemList 
                         cols='12 6' 
                         list={credits} 
