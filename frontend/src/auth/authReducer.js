6b47adb967b70538fedcaa0181611cdd29d516d6
Magno Valdetaro <mvaldetaro@gmail.com>
2018-05-03 07:59:57 -0300

    Autenticação concluída

--- frontend/src/auth/authReducer.js
+++ frontend/src/auth/authReducer.js
@@ -1,6 +1,6 @@
 const userKey = '_mymoney_user'
 const INITIAL_STATE = {
-    user: JSON.parse(localStorage.getItem(userKey)),
+    user: {name: 'Teste', email: 'aluno@teste.com'}, //JSON.parse(localStorage.getItem(userKey)),
     validToken: false
 }
 export default(state = INITIAL_STATE, action) => {
